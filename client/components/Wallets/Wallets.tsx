709f8355d2cb1e8c2c5416360af43316b4379fa8
sandeep-chahal <sandeepchahal573@gmail.com>
2022-05-12 12:53:15 +0530

    improved wallets component UI

--- client/components/Wallets/Wallets.tsx
+++ client/components/Wallets/Wallets.tsx
@@ -1,9 +1,25 @@
+import Image from "next/image";
+
 const Wallets = ({ handleConnect }: { handleConnect: (w: string) => void }) => {
 	return (
 		<ul className="wallets">
-			<li onClick={() => handleConnect("METAMASK")}>MetaMask</li>
-			<li onClick={() => handleConnect("WALLETCONNECT")}>Wallet Connect</li>
-			<li onClick={() => handleConnect("COINBASE")}>Coinbase</li>
+			<li onClick={() => handleConnect("METAMASK")}>
+				<Image width={25} height={20} src={"/metamask.svg"} alt="MetaMask" />
+				<h3>MetaMask</h3>
+			</li>
+			<li onClick={() => handleConnect("WALLETCONNECT")}>
+				<Image
+					width={25}
+					height={20}
+					src="https://raw.githubusercontent.com/WalletConnect/walletconnect-assets/492d95c038bbcde1517cab5fb90ed4514690e919/svg/original/walletconnect-logo.svg"
+					alt="Wallet Connect"
+				/>
+				<h3>Wallet Connect</h3>
+			</li>
+			<li onClick={() => handleConnect("COINBASE")}>
+				<Image width={25} height={20} src="/coinbase.svg" alt="Coinbase" />
+				<h3>Coinbase</h3>
+			</li>
 		</ul>
 	);
 };
